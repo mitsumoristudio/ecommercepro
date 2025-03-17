5136baea191d11b8e4ded8dc659122301d06ae9c
satoshimitsumori <smitsumori@hotmail.com>
2025-03-17 16:56:48 -0500

    Added Products detail page working with reduxs

---
+++ frontend/src/features/reduxslices/productsApiSlice.js
@@ -0,0 +1,24 @@
+
+import {PRODUCTS_URL} from "../../urlconstants";
+import {apiSlice} from "./apiSlice";
+
+
+// injectEndpoints- will make a request to our backend API to get all of the products. keepUnsusedDataFor keeps the data in cache for 5 secs
+export const productsApiSlice = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        getProducts: builder.query({
+            query: () => ({
+                url: PRODUCTS_URL,
+            }),
+            keepUnusedDataFor: 5,
+        }),
+        getProductDetailsById: builder.query({
+            query: (productId) => ({
+                url: `${PRODUCTS_URL}/${productId}`,
+            }),
+            keepUnusedDataFor: 5,
+        })
+    })
+});
+
+export const { useGetProductsQuery, useGetProductDetailsByIdQuery } = productsApiSlice;
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,36 +1,48 @@
+// TODO added prior to reduxtoolkit
+// import products from '../assets/mockdata/products'
+// import {useEffect, useState} from "react";
+// import axios from "axios";
 
 import React from 'react';
 import {Row, Col} from 'react-bootstrap';
-// import products from '../assets/mockdata/products'
-import {useEffect, useState} from "react";
 import Product from '../components/Product';
-import axios from "axios";
+import {useGetProductsQuery} from "../features/reduxslices/productsApiSlice";
 
 export default function HomeScreen() {
-    const [products, setProducts] = useState([]);
+    const { data: products, isLoading, isError } = useGetProductsQuery();
 
-    useEffect(() => {
-        const fetchProducts = async () => {
-            const {data} = await axios.get('/api/products');
-            setProducts(data);
-        }
-        fetchProducts();
-    },[])
+    // TODO before adding Redux/Toolkit
+    // const [products, setProducts] = useState([]);
+    //
+    // useEffect(() => {
+    //     const fetchProducts = async () => {
+    //         const {data} = await axios.get('/api/products');
+    //         setProducts(data);
+    //     }
+    //     fetchProducts();
+    // },[])
 
     return (
         <>
-            <div className={'flex-column align-items-center px-4 gap-2'}>
-        <h1 className={'py-3 px-3'}>Latest Products</h1>
-            <Row>
-                {products.map(product => {
-                    return (
-                        <Col key={product._id} sm={12} md={6} lg={5} xl={4}>
-                            <Product product={product} />
-                        </Col>
-                    )
-                })}
-            </Row>
-        </div>
+            {isLoading ? (<h2>Loading...</h2>) : isError ? (<div>{isError?.data?.message || isError.error}</div>) : ( // ? for undefined
+                <>
+                <div className={'flex-column align-items-center px-4 gap-2'}>
+                <h1 className={'py-3 px-3'}>Latest Products</h1>
+                <Row>
+                    {products.map(product => {
+                        return (
+                            <Col key={product._id} sm={12} md={6} lg={5} xl={4}>
+                                <Product product={product}/>
+                            </Col>
+                        )
+                    })}
+                </Row>
+            </div>
+                </>
+
+    )}
+
+
         </>
     )
 }
