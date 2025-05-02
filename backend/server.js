c5d433df694e3653bd0644264f94a6a3afb6eada
satoshimitsumori <smitsumori@hotmail.com>
2025-05-01 21:07:02 -0500

    deploy to production

--- backend/server.js
+++ backend/server.js
@@ -50,7 +50,7 @@ const __dirname = path.resolve(); // SET _dirname to current directory
 app.use(`/uploads`, express.static(path.join(__dirname, `../uploads`))); // changed the pathname because the root folder would not accept /uploads
 
 // Prepare for production
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV  === "production") {
     // set static folder
     app.use(express.static(path.join(__dirname, "/frontend/build")));
 
